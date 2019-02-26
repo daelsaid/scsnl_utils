8eb68f3316c1d783f8878844d9d72c996264c9a4
daelsaid <daelsaid@stanford.edu>
2019-02-25 18:36:51 -0800

    updated functions and draft of usage

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# scsnl imaging functions
+# scsnl processing functions
+
+
+
+####in draft mode
+
+`load_preproc_modules`
+`create_new_subject_list [pid] [visit] [session]`
+`add_new_scan_pid_behav [pid] [visit] [session]`
+`create_behav_training_group_csv [scan_id] [group]`
+`create_mrisubj_run_list [run1] [run2] [run3] [...]`
+`create_taskdesign_runlists [run1] [run2] [run3] [...]` _#resting excluded if listed_
+`create_main_subjectlist [pid] [visit] [session]`
+`create_spgr_subjlist_csv [pid] [visit] [session]`
+`create_spgr_names_list [# of spgr names to add]`
+`compile_behav_and_movementstats [project dir]`
+
+
+#### steps
+1. `mlsubmit_owners preprocessfmri.m preprocessfmri_config.m`
+1. `mlsubmit_owners preprocessmri_T1.m preprocessmri_config.m`
+1. `mlsubmit_owners preprocessmri_T2.m preprocessmri_config.m`
+
+2. `mlsubmit_owners movementstats.m movementstatsfmri_config.m`
--- mri_preproccessing_fxn.sh
+++ mri_preproccessing_fxn.sh
@@ -1,13 +1,11 @@
 
-
 #mri_preproc
 function load_preproc_modules(){
     ml biology fsl
     ml R/3.5.1
 }
 
-
-function add_new_pid_behav() {
+function add_new_scan_pid_behav() {
     pid=$1
     visit=$2
     session=$3
@@ -17,35 +15,25 @@ function add_new_pid_behav() {
 
     cat ${behav_proj_dir}/subjectlist.csv >> ${compiled_subj_files}/compiled_behav_sublist.csv;
 
-    rm ${behav_proj_dir}/subjectlist.csv;
+    rm ${behav_proj_dir}/subjectlist.csv 2>/dev/null ;
 
     echo -e "PID,visit,session" >> ${behav_proj_dir}/subjectlist.csv;
     echo -e "${pid}","${visit}","${session}" >> ${behav_proj_dir}/subjectlist.csv
 }
 
 
-
-function run_behav() {
-    user=$1
-    proj=$2
-
-    ml R;
-    Rscript /oak/stanford/groups/menon/projects/${user}/${proj}/scripts/behavioral/process_behavioral.R
-}
-
-function add_scanid_and_group() {
+function create_behav_training_group_csv() {
     scanid=$1
     group=$2
-    behav_proj_dir='/oak/stanford/groups/menon/projects/daelsaid/2019_met/scripts/behavioral'
 
+    behav_proj_dir='/oak/stanford/groups/menon/projects/daelsaid/2019_met/scripts/behavioral'
     compiled_subj_files='/oak/stanford/groups/menon/projects/daelsaid/2019_met/compiled_subjlists'
 
-    cat ${behav_proj_dir}/training_group.csv | grep -v "scanid,training_group" >> ${compiled_subj_files}/compiled_training_group.csv
+    sed 1d ${behav_proj_dir}/training_group.csv >> ${compiled_subj_files}/compiled_training_group.csv
 
-    rm ${behav_proj_dir}/training_group.csv
+    rm ${behav_proj_dir}/training_group.csv 2>/dev/null;
 
     echo -e "scanid,training_group" >> ${behav_proj_dir}/training_group.csv
-
     echo -e "${scanid}","${group}" >> ${behav_proj_dir}/training_group.csv
 
 }
@@ -55,10 +43,9 @@ function create_mrisubj_run_list(){
 
     datapath='/oak/stanford/groups/menon/projects/daelsaid/2019_met/data/subjectlist/'
 
-    rm ${datapath}/run_list.txt;
+    rm ${datapath}/run_list.txt 2>/dev/null ;
 
     for run in $runs; do echo -e "${run}"; done >> ${datapath}/run_list.txt
-
 }
 
 
@@ -67,42 +54,74 @@ function create_taskdesign_runlists(){
 
     datapath='/oak/stanford/groups/menon/projects/daelsaid/2019_met/data/subjectlist/'
 
-    rm ${datapath}/runlist_taskdesign.txt;
-    rm ${datapath}/runlist_grid.txt;
-    rm ${datapath}/runlist_sym.txt;
+    rm ${datapath}/runlist_taskdesign.txt 2>/dev/null ;
+    rm ${datapath}/runlist_grid.txt 2>/dev/null ;
+    rm ${datapath}/runlist_sym.txt 2>/dev/null ;
 
-    for run in $runs; do echo -e "${run}"; done >> ${datapath}/runlist_taskdesign.txt;
+    for run in $runs; do echo -e "${run}" | grep -v resting; done >> ${datapath}/runlist_taskdesign.txt;
 
     grep 'sym*' runlist_taskdesign.txt >> ${datapath}/runlist_sym.txt
     grep 'grid'* runlist_taskdesign.txt >> ${datapath}/runlist_grid.txt
+}
+
+function create_main_subjectlist(){
+    pid=$1
+    visit=$2
+    session=$3
+
+	subjectlist_path="/oak/stanford/groups/menon/projects/daelsaid/2019_met/data/subjectlist";
+    compiled_subj_files="/oak/stanford/groups/menon/projects/daelsaid/2019_met/compiled_subjlists";
+
+    # append subject lines to compiled csv before rm file and cvreating a new one
+    sed 1d ${subjectlist_path}/subjectlist.csv >> ${compiled_subj_files}/compiled_subjectlist.csv;
+
+    rm ${subjectlist_path}/subjectlist.csv 2>/dev/null ;
+
+    echo -e "PID,visit,session" >> ${subjectlist_path}/subjectlist.csv;
+    echo -e "${pid}","${visit}","${session}" >> ${subjectlist_path}/subjectlist.csv
+
 }
 
 function create_spgr_subjlist_csv(){
     pid=$1
     visit=$2
     session=$3
-    subjectlist_path=$4
 
-    compiled_subj_files='/oak/stanford/groups/menon/projects/daelsaid/2019_met/compiled_subjlists'
+	subjectlist_path="/oak/stanford/groups/menon/projects/daelsaid/2019_met/data/subjectlist";
+    compiled_subj_files="/oak/stanford/groups/menon/projects/daelsaid/2019_met/compiled_subjlists";
 
     # append subject lines to compiled csv before rm file and cvreating a new one
-    cat ${subjectlist_path}/spgrsubjectlist.csv | grep -v "PID,visit,session" >> ${compiled_subj_files}/compiled_spgrsubjectlist.csv;
+    sed 1d ${subjectlist_path}/spgr_subjectlist.csv >> ${compiled_subj_files}/compiled_spgrsubjectlist.csv;
 
-    rm ${subjectlist_path}/spgrsubjectlist.csv ;
+    rm ${subjectlist_path}/spgr_subjectlist.csv 2>/dev/null ;
 
-    echo -e "${pid}","${visit}","${session}" >> ${subjectlist_path}/spgrsubjectlist.csv
+    echo -e "PID,visit,session" >> ${subjectlist_path}/spgr_subjectlist.csv;
+    echo -e "${pid}","${visit}","${session}" >> ${subjectlist_path}/spgr_subjectlist.csv
 
 }
 
 function create_spgr_names_list(){
     number_of_structs_to_add="$@"
 
-    datapath='/oak/stanford/groups/menon/projects/daelsaid/2019_met/data/subjectlist/'
-
-    rm ${datapath}/spgrnameslist.txt;
+    rm ${datapath}/spgrnameslist.txt 2>/dev/null ;
 
     for struct in `seq 1 "$@"`; do
-    echo -e "spgr" ; done >> ${datapath}/spgrnameslist.txt
+    echo -e "spgr" ; done >> ${datapath}/spgrnames_list.txt
+}
+
+
+function compile_behavdata_and_movementstats(){
+    project_folder=$1
+
+    compiled_output_dir=${project_folder}/compiled_subjlists
+    behav_dir=${project_folder}/scripts/behavioral
+    movementstats_dir=${project_folder}/scripts/taskfmri/movement_stats
+
+    sed 1d ${behav_dir}/data_merged_sorted.csv >> ${compiled_output_dir}/compiled_behavioral_data_merge_sorted.csv
+    sed 1d ${behav_dir}/data_summary.csv >> ${compiled_output_dir}/compiled_behav_data_summary.csv
+
+    sed 1d ${movementstats_dir}/MovementSummaryStats.txt >> ${compiled_output_dir}/compiled_movement_summary_stats.txt
+    sed 1d ${movementstats_dir}/MovementMissingInfo.txt >> ${compiled_output_dir}/compiled_movement_missing_info.txt
 }
 
 
@@ -112,4 +131,13 @@ function load_modules(){
     modules="$@"
 
     for module in ${modules}; do echo "ml"${modules};done
+}
+
+
+function run_behav() {
+    user=$1
+    proj=$2
+
+    ml R;
+    Rscript /oak/stanford/groups/menon/projects/${user}/${proj}/scripts/behavioral/process_behavioral.R
 }
