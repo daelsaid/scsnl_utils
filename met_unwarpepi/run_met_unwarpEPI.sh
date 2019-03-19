4d42a1c9528ea14198299eff224a142c55db68ca
daelsaid <daelsaid@stanford.edu>
2019-03-19 16:05:02 -0700

    final function to run unwarping on func scans for all runs

--- met_unwarpepi/run_met_unwarpEPI.sh
+++ met_unwarpepi/run_met_unwarpEPI.sh
@@ -26,20 +26,37 @@ function copy_all_func_and_struct_data(){
         cp -vr $scans /oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/${subj}_${visit}_${session}_$(basename $scans);
     done
 }
-function gunzip_and_unwarp() {
-	pid=$1
-	visit=$2
-	session=$3
 
-	gunzip ${pid}_${visit}_${session}*.nii.gz;
-	# run unwarpepi on each run + it's pepolar scan
 
+function gunzip_and_unwarp(){
+
+    Usage () {
+        echo ""
+        echo "Usage: gunzip_and_unwarp <PID> <VISIT> <SESSION>"
+        echo "PID: 4 digit SCSNL subject ID"
+        echo "VISIT: visit number"
+        echo "SESSION: session number"
+    }
+
+    if [ $# -lt 3 ] ; then
+        Usage
+    fi
+
+    pid=$1
+    visit=$2
+    session=$3
+
+    #gunzip nii.gz
+    gunzip ${pid}_${visit}_${session}*.nii.gz
+
+    # run unwarpepi on each run + it's pepolar scan
     for scan in `ls ${pid}_${visit}_${session}_sym?*.nii | sort -u` `ls ${pid}_${visit}_${session}_grid?*.nii` `ls ${pid}_${visit}_${session}_resting.nii`; do
-         subj_prefix=`echo $scan | cut -d_ -f1-4 | cut -d'.' -f1`;
-         subj_visit_date=`echo $scan | cut -d_ -f1-3`;
-         task=`echo $scan | cut -d'_' -f4 | cut -d. -f1`;
-         echo ${subj_prefix} ${task};
+        subj_prefix=`echo $scan | cut -d_ -f1-4 | cut -d'.' -f1`;
+        subj_visit_date=`echo $scan | cut -d_ -f1-3`;
+        task=`echo $scan | cut -d'_' -f4 | cut -d. -f1`;
+        struct=`ls ${subj_visit_date}_spgr* | cut -d'_' -f4`;
 
-         sbatch -p owners,menon --wrap="ml biology afni; python /oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/unwarpepi.py -f ${subj_visit_date}_${task}.nii'[0..20]' -r '${subj_prefix}_reverse.nii' -d '${subj_prefix}.nii' -a '${subj_visit_date}_spgr{1..2}' -s '${subj_prefix}'";
-     done
+        echo $subj_prefix, ${task};
+        sbatch -p owners,menon --wrap="ml biology afni; python /oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/unwarpepi.py -f ${subj_visit_date}_${task}.nii'[0..20]' -r '${subj_prefix}_reverse.nii' -d '${subj_prefix}.nii' -a '${subj_visit_date}_${struct}.nii' -s '${subj_prefix}'"
+    done
 }
